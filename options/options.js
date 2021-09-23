2ea072b1e087b0a639f8f37b3aae3e1b065a59e6
Crowdbotics <team@crowdbotics.com>
2021-09-23 03:51:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dry_truth_30764Identifier",
+  url: "https://dry_truth_30764Identifier.botics.co",
+  api: "https://dry_truth_30764Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
